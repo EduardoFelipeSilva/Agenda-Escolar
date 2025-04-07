c15ae33af2faba6b8edaecb03de007cc47c252cf
Eduardo Felipe da Silva <eduardo.silva338@fatec.sp.gov.br>
2025-04-07 16:41:39 -0300

    gg

---
+++ Horario_Escolar/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="https://fonts.gstatic.com/s/i/materialiconsoutlined/event/v10/24px.svg" type="image/svg+xml">
+    <title>Calendário Escolar</title>
+</head>
+<body>
+    <h1>Calendário Escolar 1º Semestre</h1>
+    <h2>Análise e Desenvolvimento de Sistemas</h2>
+
+    <!-- Início da Tabela de Horários -->
+    <div class="calendario">
+        <!-- Segunda-feira -->
+        <div class="grid-container">
+            <div class="dia">Segunda-Feira</div>
+            <div class="horario">07:30</div>
+            <div class="materia">Empreendedorismo</div>  
+            <div class="professor">Daisy</div>  
+            <div class="horario">08:20</div>
+            <div class="materia">Empreendedorismo</div>
+            <div class="professor">Daisy</div>  
+            <div class="horario">09:25</div>
+            <div class="materia">Inteligência Emocional e Autoconhecimento</div>
+            <div class="professor">Daisy</div>  
+            <div class="horario">10:15</div> 
+            <div class="materia">Inteligência Emocional e Autoconhecimento</div> 
+            <div class="professor">Daisy</div>  
+        </div>
+        <!-- Fim Segunda-feira -->
+
+        <!-- Terça-feira -->
+        <div class="grid-container">
+            <div class="dia">Terça-Feira</div>
+            <div class="horario">07:30</div>
+            <div class="materia">Algoritmos e Lógica de Programação</div>  
+            <div class="professor">Marcia</div>  
+            <div class="horario">08:20</div>
+            <div class="materia">Algoritmos e Lógica de Programação</div>
+            <div class="professor">Marcia</div>  
+            <div class="horario">09:25</div>
+            <div class="materia">Algoritmos e Lógica de Programação</div>
+            <div class="professor">Marcia</div>  
+            <div class="horario">10:15</div> 
+            <div class="materia">Algoritmos e Lógica de Programação</div> 
+            <div class="professor">Marcia</div>  
+        </div>
+        <!-- Fim Terça-feira -->
+
+        <!-- Quarta-feira -->
+        <div class="grid-container">
+            <div class="dia">Quarta-Feira</div>
+            <div class="horario">07:30</div>
+            <div class="materia">Arquitetura e Organização de Computadores</div>  
+            <div class="professor">Rogerio</div>  
+            <div class="horario">08:20</div>
+            <div class="materia">Arquitetura e Organização de Computadores</div>
+            <div class="professor">Rogerio</div>  
+            <div class="horario">09:25</div>
+            <div class="materia">Arquitetura e Organização de Computadores</div>
+            <div class="professor">Rogerio</div>  
+            <div class="horario">10:15</div> 
+            <div class="materia">Arquitetura e Organização de Computadores</div> 
+            <div class="professor">Rogerio</div>  
+        </div>
+        <!-- Fim Quarta-feira -->
+
+        <!-- Quinta-feira -->
+        <div class="grid-container">
+            <div class="dia">Quinta-Feira</div>
+            <div class="horario">07:30</div>
+            <div class="materia">Matemática Discreta</div>  
+            <div class="professor">Claudia</div>  
+            <div class="horario">08:20</div>
+            <div class="materia">Matemática Discreta</div>
+            <div class="professor">Claudia</div>  
+            <div class="horario">09:25</div>
+            <div class="materia">Matemática Discreta</div>
+            <div class="professor">Claudia</div>  
+            <div class="horario">10:15</div> 
+            <div class="materia">Matemática Discreta</div> 
+            <div class="professor">Claudia</div>  
+        </div>
+        <!-- Fim Quinta-feira -->
+
+        <!-- Sexta-feira -->
+        <div class="grid-container">
+            <div class="dia">Sexta-Feira</div>
+            <div class="horario">07:30</div>
+            <div class="materia">Introdução ao Desenvolvimento de Sistemas</div>  
+            <div class="professor">Leandro</div>  
+            <div class="horario">08:20</div>
+            <div class="materia">Introdução ao Desenvolvimento de Sistemas</div>
+            <div class="professor">Leandro</div>  
+            <div class="horario">09:25</div>
+            <div class="materia">Introdução ao Desenvolvimento de Sistemas</div>
+            <div class="professor">Leandro</div>  
+            <div class="horario">10:15</div> 
+            <div class="materia">Introdução ao Desenvolvimento de Sistemas</div> 
+            <div class="professor">Leandro</div>  
+            <div class="horario">11:20</div>
+            <div class="materia">Projeto Integrador</div>
+            <div class="professor">Marcia</div>  
+            <div class="horario">12:10</div> 
+            <div class="materia">Projeto Integrador</div> 
+            <div class="professor">Marcia</div>  
+        </div>
+        <!-- Fim Sexta-feira -->
+    </div>
+    <!-- Fim da Tabela de Horários -->
+</body>
+</html>
